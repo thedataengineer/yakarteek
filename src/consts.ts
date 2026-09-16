de517bd23d8ff800cdabfb58a7183d06dad7aecf
Aswani Karteek Yadavilli <21700785+thedataengineer@users.noreply.github.com>
2026-09-15 21:32:01 -0400

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Aswani Karteek Yadavilli';
+export const SITE_DESCRIPTION =
+  'Data engineer, speaker, and advisor. Book me for keynotes, consulting, training, mentorship, and more.';
